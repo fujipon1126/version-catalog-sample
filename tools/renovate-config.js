bd42be5c43bc36a3b648dc82c87a98c31399f7f1
fujipon1126 <fujitansp@gmail.com>
2022-10-31 23:54:22 +0900

    update renovate-bot.yml and renovate-config.js

---
+++ tools/renovate-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  branchPrefix: 'develop',
+  dryRun: false,
+  username: 'renovate-release',
+  gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+  platform: 'github',
+  repositories: [
+    'fujipon1126/version-catalog-sample'
+  ],
+  packageRules: [
+    {
+      matchUpdateTypes: [
+        'pin',
+        'digest',
+        'patch',
+        'minor',
+        'major',
+        'lockFileMaintenance',
+      ],      
+    }
+  ],
+  extends: [
+    "config:base"
+  ]
+};
